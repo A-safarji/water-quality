bcccdecd5d49985ab26a26e13c8f70d8db87da45
A.Safarji <a.m.safarji@gmail.com>
2021-11-03 17:16:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ else:
 st.write('* 0 = not safe, 1= safe')
 
 st.write('---')
-explainer  = shap.TreeExplainer(load_clf)
+explainer  = shap.TreeExplainer(prediction)
 shap_values = explainer.shap_values(input_params)
 
 st.header('Feature Importance')
