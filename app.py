a51e57e4c8349330529d103d3f3562861e9f3cdc
A.Safarji <a.m.safarji@gmail.com>
2021-11-03 17:29:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -99,15 +99,5 @@ st.write('* 0 = not safe, 1= safe')
 
 st.write('---')
 
-explainer = shap.TreeExplainer(load_clf)
-shap_values = explainer.shap_values(df)
-
-st.header('Feature Importance')
-st.write('* SHAPE values show how much a given feature changed our prediction')
-plt.title('Feature importance based on SHAP values')
-shap.summary_plot(shap_values, df)
-st.pyplot(bbox_inches='tight')
-st.write('---')
-
 shap.plots.force(prediction[0])
 
