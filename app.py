9b6454ee49bd845cbc824639b29421ceecb321b0
Abdultawwab Safarji <a.m.safarji@gmail.com>
2022-09-12 22:46:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -54,63 +54,63 @@ st.sidebar.title("Select your Input  Values")
 
 uploaded_file=st.sidebar.file_uploader("Upload your csv file in the same input as the example csv file",type=["csv"])
 
-# if uploaded_file is not None:
-# 	input_params=pd.read_csv(uploaded_file)
+if uploaded_file is not None:
+	input_params=pd.read_csv(uploaded_file)
 	
 
-# else:
-# 	ph=st.sidebar.slider("ph value",0.1,28.3,7.5)
-# 	Hardness=st.sidebar.slider("Hardness value",47.432,323.3,118.2)
-# 	Solids=st.sidebar.slider("Solids value(mg/L) ",181.4,30000.0,14285.58)
-# 	Chloramines=st.sidebar.slider("Chloramines value",0.1,28.3,9.27)
-# 	Sulfate=st.sidebar.slider("Sulfate value(mg/L)",47.432,400.3,333.07)
-# 	Conductivity=st.sidebar.slider("Conductivity value(μS/cm)",181.4,753.2,418.60)
-# 	Organic_carbon=st.sidebar.slider("Organic Carbon value(mg/L)",2.1,28.3,16.86)
-# 	Trihalomethanes=st.sidebar.slider("Trihalomethanesvalue(ppm)",47.432,323.3,66.42)
-# 	Turbidity=st.sidebar.slider("Turbidity value(NTU)",0.1,28.3,3.05)
+else:
+	ph=st.sidebar.slider("ph value",0.1,28.3,7.5)
+	Hardness=st.sidebar.slider("Hardness value",47.432,323.3,118.2)
+	Solids=st.sidebar.slider("Solids value(mg/L) ",181.4,30000.0,14285.58)
+	Chloramines=st.sidebar.slider("Chloramines value",0.1,28.3,9.27)
+	Sulfate=st.sidebar.slider("Sulfate value(mg/L)",47.432,400.3,333.07)
+	Conductivity=st.sidebar.slider("Conductivity value(μS/cm)",181.4,753.2,418.60)
+	Organic_carbon=st.sidebar.slider("Organic Carbon value(mg/L)",2.1,28.3,16.86)
+	Trihalomethanes=st.sidebar.slider("Trihalomethanesvalue(ppm)",47.432,323.3,66.42)
+	Turbidity=st.sidebar.slider("Turbidity value(NTU)",0.1,28.3,3.05)
 
-# 	dict_values={"ph":ph, "Hardness":Hardness, "Solids":Solids,"Chloramines":Chloramines,"Sulfate":Sulfate,
-# 		     "Conductivity":Conductivity,"Organic_carbon":Organic_carbon,
-# 		     "Trihalomethanes":Trihalomethanes,"Turbidity":Turbidity}
-# 	features=pd.DataFrame(dict_values,index=[0])
-# 	input_params=features
+	dict_values={"ph":ph, "Hardness":Hardness, "Solids":Solids,"Chloramines":Chloramines,"Sulfate":Sulfate,
+		     "Conductivity":Conductivity,"Organic_carbon":Organic_carbon,
+		     "Trihalomethanes":Trihalomethanes,"Turbidity":Turbidity}
+	features=pd.DataFrame(dict_values,index=[0])
+	input_params=features
 
-if uploaded_file is not None:
+# if uploaded_file is not None:
 
-      bytesData = pd.read_csv(uploaded_file) 
+#       bytesData = pd.read_csv(uploaded_file) 
       
       
-      st.write(bytesData)
-# 		uploaded_file
-# 		data = pd.read_csv(uploaded_file)
-# 		data	
+#       st.write(bytesData)
+# # 		uploaded_file
+# # 		data = pd.read_csv(uploaded_file)
+# # 		data	
 
-check_list = ['ph','Hardness','Solids','Chloramines','Sulfate','Conductivity','Organic_carbon','Trihalomethanes'	
-	 ,'Turbidity']
+# check_list = ['ph','Hardness','Solids','Chloramines','Sulfate','Conductivity','Organic_carbon','Trihalomethanes'	
+# 	 ,'Turbidity']
 
-df_cols = pd.read_csv("example.csv", nrows=0)
+# df_cols = pd.read_csv("example.csv", nrows=0)
 
-df_cols_list = bytesData.columns.tolist()
+# df_cols_list = bytesData.columns.tolist()
 
-#df_cols_list == check_list, "Columns are misaligned: {0} vs {1}".format(df_cols_list, check_list)
-if (df_cols_list != check_list):
-	st.write('not matched')
-if (df_cols_list == check_list):
-	st.write('matched you can uploaded the file')
+# #df_cols_list == check_list, "Columns are misaligned: {0} vs {1}".format(df_cols_list, check_list)
+# if (df_cols_list != check_list):
+# 	st.write('not matched')
+# if (df_cols_list == check_list):
+# 	st.write('matched you can uploaded the file')
 
 
 
 
 # #ph Solids Chloramines Sulfate Trihalomethanes Turbidity
 
-# st.subheader("Your selected input fields")
-# # if st.button('save dataframe'):
-# #     open('input_params.csv', 'w').write(input_params.to_csv())
-# if uploaded_file is not None:
-# 	st.write(input_params)
-# 	#input_params.to_csv(index=False)
-# else:
-# 	st.write(input_params)
+st.subheader("Your selected input fields")
+# if st.button('save dataframe'):
+#     open('input_params.csv', 'w').write(input_params.to_csv())
+if uploaded_file is not None:
+	st.write(input_params)
+	#input_params.to_csv(index=False)
+else:
+	st.write(input_params)
 
 	
 
@@ -156,3 +156,108 @@ else:
 st.write('* Thanks for useing AKN Water Quality ')
 
 st.write('---')
+
+
+
+
+
+
+# import streamlit as st
+# import pandas as pd
+# import numpy as np
+# import pickle
+# from pycaret.classification import *
+# import matplotlib.pyplot as plt
+# from io import StringIO
+
+
+
+# #st.markdown('<p align="center"> Water Quality **Estimation** </p>', unsafe_allow_html=True))
+
+# st.markdown(' <p align="center" class="big-font">  <b>Water Quality <u>Check</b> 🌟 </p>', unsafe_allow_html=True)	
+
+
+# st.markdown("""
+# <style>
+# .big-font {
+#     font-size:45px !important;
+# }
+# </style>
+# """, unsafe_allow_html=True)
+
+# #st.markdown('<p class="big-font">Hello World !!</p>', unsafe_allow_html=True)
+ 
+# st.markdown(' <p align="center"><img width="523" src="https://user-images.githubusercontent.com/20365333/140042600-a602ed75-6571-4f7b-adee-0d33d51f9cf0.jpg"></p>', unsafe_allow_html=True)	
+
+# #st.write(""" """)
+
+# st.markdown("""
+# 	AKN is a material testing laboratory that focuses on water quality tests and classification. Our next step,
+# 	to expand the business research and development that to start a project to automate the water classification process.
+# 	Access to safe drinking-water is essential to health, a basic human right and a component of effective policy for health protection.
+
+# 	""")
+
+# st.subheader("Sample values for the input")
+# df=pd.read_csv("example.csv")
+# #df.drop('Unnamed: 0', axis=1, inplace=True)
+# df
+
+# #df_example=df.iloc[df['ph']==6.007427,['Organic_carbon','Conductivity','Hardness']]
+# #df_example=df.iloc[0,1:]
+# #df_example
+
+# if st.checkbox("Show orignal dataframe"):
+# 	dataframe=pd.read_csv("waterNew.csv")
+# 	dataframe.drop('Unnamed: 0', axis=1, inplace=True)
+# 	dataframe
+
+# ##Sidebar
+
+# st.sidebar.title("Select your Input  Values")
+
+# uploaded_file=st.sidebar.file_uploader("Upload your csv file in the same input as the example csv file",type=["csv"])
+
+# # if uploaded_file is not None:
+# # 	input_params=pd.read_csv(uploaded_file)
+	
+
+# # else:
+# # 	ph=st.sidebar.slider("ph value",0.1,28.3,7.5)
+# # 	Hardness=st.sidebar.slider("Hardness value",47.432,323.3,118.2)
+# # 	Solids=st.sidebar.slider("Solids value(mg/L) ",181.4,30000.0,14285.58)
+# # 	Chloramines=st.sidebar.slider("Chloramines value",0.1,28.3,9.27)
+# # 	Sulfate=st.sidebar.slider("Sulfate value(mg/L)",47.432,400.3,333.07)
+# # 	Conductivity=st.sidebar.slider("Conductivity value(μS/cm)",181.4,753.2,418.60)
+# # 	Organic_carbon=st.sidebar.slider("Organic Carbon value(mg/L)",2.1,28.3,16.86)
+# # 	Trihalomethanes=st.sidebar.slider("Trihalomethanesvalue(ppm)",47.432,323.3,66.42)
+# # 	Turbidity=st.sidebar.slider("Turbidity value(NTU)",0.1,28.3,3.05)
+
+# # 	dict_values={"ph":ph, "Hardness":Hardness, "Solids":Solids,"Chloramines":Chloramines,"Sulfate":Sulfate,
+# # 		     "Conductivity":Conductivity,"Organic_carbon":Organic_carbon,
+# # 		     "Trihalomethanes":Trihalomethanes,"Turbidity":Turbidity}
+# # 	features=pd.DataFrame(dict_values,index=[0])
+# # 	input_params=features
+
+# if uploaded_file is not None:
+
+#       bytesData = pd.read_csv(uploaded_file) 
+      
+      
+#       st.write(bytesData)
+# # 		uploaded_file
+# # 		data = pd.read_csv(uploaded_file)
+# # 		data	
+
+# check_list = ['ph','Hardness','Solids','Chloramines','Sulfate','Conductivity','Organic_carbon','Trihalomethanes'	
+# 	 ,'Turbidity']
+
+# df_cols = pd.read_csv("example.csv", nrows=0)
+
+# df_cols_list = bytesData.columns.tolist()
+
+# #df_cols_list == check_list, "Columns are misaligned: {0} vs {1}".format(df_cols_list, check_list)
+# if (df_cols_list != check_list):
+# 	st.write('not matched')
+# if (df_cols_list == check_list):
+# 	st.write('matched you can uploaded the file')
